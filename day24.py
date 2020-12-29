550eae43bdfa60a729bf2be89a5b83a81fa6ec25
pbednarz <philip.bednarz@stanfordalumni.org>
2020-12-29 10:39:24 -0800

    Clean up formatting.

--- day24.py
+++ day24.py
@@ -1,40 +1,39 @@
-dxy = {'e':(2,0), 'ne':(1,1), 'se':(1,-1), 'w':(-2,0), 'nw':(-1,1), 'sw':(-1,-1)}
-blktiles = set()
+dxy = {'e': (2, 0), 'ne': (1, 1), 'se': (1, -1), 'w': (-2, 0), 'nw': (-1, 1), 'sw': (-1, -1)}
+btiles = set()      # set of coordinates for all black tiles
 
 with open('day24.txt') as f:
     for line in f:
-        dir = ''
-        p = (0,0)
+        d, p = '', (0, 0)
         for c in line.strip():
+            d += c
             if c in 'ns':
-                dir += c
                 continue
-            dir += c
-            p = tuple(map(lambda x,y:x+y, dxy[dir], p))
-            dir = ''
-        blktiles ^= {p}
-print('Part 1:', len(blktiles))
+            p = tuple(map(lambda x, y: x+y, dxy[d], p))
+            d = ''
+        btiles ^= {p}
+print('Part 1:', len(btiles))
 
-def flipfloor(blktiles):
-    newblktiles = set()
-    blkneighbors = {}
-    for (x,y) in blktiles:
+
+def flipfloor(btiles0):       # set of current black tiles
+    btiles1 = set()           # new set of black tiles after one round
+    wtiles = {}               # white tiles with black neighbors, count of black neighbors
+    for (x, y) in btiles0:
         cnt = 0
-        for xf,yf in [(x+2,y), (x+1,y+1), (x+1,y-1), (x-2,y), (x-1,y+1), (x-1,y-1)]:
-            if (xf,yf) in blktiles:
+        for dp in [(x+2, y), (x+1, y+1), (x+1, y-1), (x-2, y), (x-1, y+1), (x-1, y-1)]:
+            if dp in btiles0:
                 cnt += 1
             else:
-                if (xf,yf) not in blkneighbors:
-                    blkneighbors[(xf,yf)] = 0
-                blkneighbors[(xf,yf)] += 1
+                if dp not in wtiles:
+                    wtiles[dp] = 0
+                wtiles[dp] += 1
         if cnt == 1 or cnt == 2:
-            newblktiles.add((x,y))
-    for (x,y), cnt in blkneighbors.items():
+            btiles1.add((x, y))
+    for p, cnt in wtiles.items():
         if cnt == 2:
-            newblktiles.add((x,y))
-    return newblktiles
+            btiles1.add(p)
+    return btiles1
+
 
 for _ in range(100):
-    blktiles = flipfloor(blktiles)
-print('Part 2:', len(blktiles))
-
+    btiles = flipfloor(btiles)
+print('Part 2:', len(btiles))
